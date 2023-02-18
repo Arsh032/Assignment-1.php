ca6fbc744c9525c34151c12547c11cdbeb144726
Arsh032 <122288475+Arsh032@users.noreply.github.com>
2023-02-18 17:29:53 -0500

    Add files via upload

---
+++ fieldatc/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Majha college Website </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+		<?php
+		include 'content.php';
+	?>
+
+	<?php
+	include 'footer.php';
+	?>
+</body>
+</html>
